436cbd974cbfa6694cd9a5c2e5f40f0b0fbbd580
Rachel Fish <itsrachelfish@gmail.com>
2014-12-30 12:20:58 -0700

    Require.js actually works now

--- scripts/main.js
+++ scripts/main.js
@@ -1,28 +1,37 @@
-$('.gallery').hbGallery({
-    children: '.thumbnail',
-    parent: '.overlay img',
-    animated: function()
+require(["jquery", "app/component", "ui/hbGallery"], function($, component)
+{
+    $(document).ready(function()
     {
-        $('.nav input').prop('disabled', false);
-    }
+        $('.gallery').hbGallery(
+        {
+            children: '.thumbnail',
+            parent: '.overlay img',
+            animated: function()
+            {
+                $('.nav input').prop('disabled', false);
+            }
+        });
+
+        $('body').on('click', '.gallery .thumbnail', function()
+        {
+            $('.overlay').fadeIn();
+
+            // Hide the previous image when opening the overlay
+            $('.gallery').trigger('gallery-hide');
+            $('.gallery').trigger('gallery-show', $(this).index());
+        });
+
+        $('body').on('click', '.nav input', function()
+        {
+            $('.gallery').trigger('gallery-'+this.dataset.value);
+            $('.nav input').prop('disabled', true);                    
+        });
+
+        $('body').on('click', '.overlay .close', function()
+        {
+            $('.overlay').fadeOut();
+        });
+    });
 });
 
-$('body').on('click', '.gallery .thumbnail', function()
-{
-    $('.overlay').fadeIn();
 
-    // Hide the previous image when opening the overlay
-    $('.gallery').trigger('gallery-hide');
-    $('.gallery').trigger('gallery-show', $(this).index());
-});
-
-$('body').on('click', '.nav input', function()
-{
-    $('.gallery').trigger('gallery-'+this.dataset.value);
-    $('.nav input').prop('disabled', true);                    
-});
-
-$('body').on('click', '.overlay .close', function()
-{
-    $('.overlay').fadeOut();
-});
