bd24b273689954d7bc516aa2a2b508c3edb061f3
michaelroth <mroth@highbridgecreative.com>
2014-12-31 12:15:44 -0700

    added versioning to css/js minification

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,54 +1,62 @@
 module.exports = function(grunt) {
-
-  'use strict';
-
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    sass: {
-      dist: {
-        options : {
-          loadPath: require('node-bourbon').includePaths,
-          style : 'compressed'
+    'use strict';
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        sass: {
+            dev: {
+                options: {
+                    loadPath: require('node-bourbon').includePaths
+                },
+                files: {
+                    'styles/css/main.css': 'styles/sass/main.scss'
+                }
+            },
+            prod: {
+                options: {
+                    loadPath: require('node-bourbon').includePaths,
+                    style: 'compressed'
+                },
+                files: {
+                    'styles/css/main-<%= pkg.version %>.css': 'styles/sass/main.scss'
+                }
+            }
         },
-        files: {
-          'styles/css/main.css' : 'styles/sass/main.scss'
+        watch: {
+            css: {
+                files: '**/*.scss',
+                tasks: ['sass:dev']
+            }
+        },
+        requirejs: {
+            compile: {
+                options: {
+                    name: 'config',
+                    baseUrl: 'scripts/',
+                    mainConfigFile: 'scripts/config.js',
+                    out: 'scripts/main-<%= pkg.version %>.js',
+                    findNestedDependencies: true,
+                    preserveLicenseComments: false,
+                    include: ['vendor/require.js']
+                }
+            }
+        },
+        connect: {
+            server: {
+                options: {
+                    port: 8080
+                }
+            }
         }
-      }
-    },
-    watch: {
-      css: {
-        files: '**/*.scss',
-        tasks: ['sass']
-      }
-    },
-    requirejs: {
-      compile: {
-        options: {
-          name: "config",
-          baseUrl: "scripts/",
-          mainConfigFile: "scripts/config.js",
-          out: "scripts/main.min.js",
-          findNestedDependencies: true,
-          preserveLicenseComments: false,
-          include: ['vendor/require.js']
-        }
-      }
-    },
-    connect: {
-      server: {
-        options: {
-          port: 8080
-        }
-      }
-    }
-  });
+    });
 
-  grunt.loadNpmTasks('grunt-contrib-sass');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-requirejs');
-  grunt.loadNpmTasks('grunt-contrib-connect');
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-requirejs');
+    grunt.loadNpmTasks('grunt-contrib-connect');
 
-  grunt.registerTask('default', ['connect','watch']);
-  grunt.registerTask('prod', ['sass','requirejs']);
+    // default grunt task
+    grunt.registerTask('default', ['connect', 'watch']);
 
+    // prod release
+    grunt.registerTask('prod', ['sass:prod', 'requirejs']);
 }
